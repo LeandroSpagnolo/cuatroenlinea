13fc2ce098dd42e453c4a2364091d9b496b04503
Leandro <leandr0fox561@gmail.com>
2022-08-28 14:56:15 -0300

    agregadas funciones para ganadores en eje X e Y

--- app/Ganador.php
+++ app/Ganador.php
@@ -7,8 +7,50 @@ include 'Tablero.php';
 
 interface interfazGanador{
 
-    public function ganadorEjex() : bool; // devuelve True o False dependiendo si hay un ganador en el eje X
-    public function ganadorEjey() : bool; // devuelve True o False dependiendo si hay un ganador en el eje Y
-    public function ganadorDiagonal() : bool; // devuelve True o False dependiendo si hay un ganador en la diagonal
+    public function ganadorEjex(Tablero $tablero, int $x, int $y) : bool; // devuelve True o False dependiendo si hay un ganador en el eje X
+    public function ganadorEjey(Tablero $tablero, int $x, int $y) : bool; // devuelve True o False dependiendo si hay un ganador en el eje Y
+    public function ganadorDiagonal(Tablero $tablero, int $x, int $y) : bool; // devuelve True o False dependiendo si hay un ganador en la diagonal
     
+}
+
+class Ganador implements interfazGanador{
+
+    public function ganadorEjeX(Tablero $tablero, int $x, int $y){
+
+        $ficha = $tablero->devolverValorCasilla($x, $y);
+        $contador = 0;
+        for($i = 0; $i < $tablero->dimensionTableroX(); $i++){
+            if($ficha == $tablero->devolverValorCasilla($i, $y)){
+                $contador++;
+            }
+        }
+        if($contador == 4){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public function ganadorEjeY(Tablero $tablero, int $x, int $y){
+        $ficha = $tablero->devolverValorCasilla($x, $y);
+        $contador = 0;
+        for($i = 0; $i < $tablero->dimensionTableroY(); $i++){
+            if($ficha == $tablero->devolverValorCasilla($x, $i)){
+                $contador++;
+            }
+        }
+        if($contador == 4){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    
+
+    
+
+
 }
